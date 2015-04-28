e50a498656c6309c408a8dbf144cc45a83684ac0
Ephraim <ephraimfields@gmail.com>
2015-04-28 14:16:54 -0400

    Initial Init

---
+++ Space Shooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+	public float lifetime;
+
+	void Star ()
+	{
+		Destroy (gameObject, lifetime);
+	}
+}
